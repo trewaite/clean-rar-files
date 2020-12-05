1888225b832868d9b51cdb5e3ec294433d7956ea
Trevor-Waite <trevor.anthony.waite@gmail.com>
2020-12-05 15:25:42 -0500

    update to sh

--- bin/clean_video.sh
+++ bin/clean_video.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+# your video file directory
 echo Directory from Root:
 read direc
 
+# ['r']
 echo List of file extensions to delete:
 read del_extensions 
 
+# type 'video' for video type
 echo Type of file to keep:
 read keep_str
 
